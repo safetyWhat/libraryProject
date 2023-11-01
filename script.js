7288efbbe5135f2fafd65da8d46e172b0f7a5ea6
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-11-01 17:14:53 -0400

    Create js to populate hmtl info for book title on tiles.

--- script.js
+++ script.js
@@ -9,45 +9,46 @@ let newPages = document.getElementById("pages");
 let newReadStatus = document.getElementById("read");
 const container = document.querySelector('.container');
 
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.changeStatus = function(status) {
+    this.changeStatus = function(status) { //Need this function to be able to change status in the future.
       this.read = status
     }
   }
 
-  function addBookToLibrary(title, author, pages, read) {
-    console.log(myLibrary.length);
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook)
-    console.log(myLibrary.length);
-    const bookBox = document.createElement('div');
-    container.append(bookBox);
-    bookBox.setAttribute('class', 'bookBox');
-    bookBox.setAttribute('id', 'book' + myLibrary.length);
-  }
+function addBookToLibrary(title, author, pages, read) {
+  console.log(myLibrary.length);
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook)
+  console.log(myLibrary.length);
+  const bookBox = document.createElement('div'); //Create div for the book details to land in.
+  container.append(bookBox);
+  bookBox.setAttribute('class', 'bookBox');
+  bookBox.setAttribute('id', 'book' + myLibrary.length); //Assign id for which book it is for possible deletion later.
+  bookBox.innerHTML = `
+    <h3 class="bookTitle"> ${title} </h3>
+    <p class="bookAuthor"> ${author} </p>
+    <p class="bookPages">${pages}</p>
+    <button class="bookStatusChange">${read}</button>
+    <button class="bookRemove">Delete</button>`; 
+}
   
-  addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 pages", "Read")
-  addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "Too Many", "Reading")
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 pages", "Read")
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "Too Many", "Reading")
 
-  console.log(myLibrary)
+console.log(myLibrary)
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-  
-  
-
-  showButton.addEventListener("click", () => {
-    dialog.showModal();
-  });
-
-  document.getElementById('submit').addEventListener('click', function(event) {
-      event.preventDefault();
-      dialog.close();
-      addBookToLibrary(newTitle.value , newAuthor.value, newPages.value, newReadStatus.value);
-      console.log(myLibrary);
-      newBookForm.reset();
-    })
+document.getElementById('submit').addEventListener('click', function(event) {
+  event.preventDefault();
+  dialog.close();
+  addBookToLibrary(newTitle.value , newAuthor.value, newPages.value, newReadStatus.value);
+  console.log(myLibrary);
+  newBookForm.reset();
+})
    
