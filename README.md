e5191695cd5cb4967256c0d8197a7918a402a5d1
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-11-07 12:44:58 -0500

    Create js to remove book info from Library array. Still need to add js to delete visual representation of book info.

--- README.md
+++ README.md
@@ -12,13 +12,18 @@ Add Book:
 onClick - pop up to complete book info (use the HTML dialogue element to do this using a modal. In js -> HTMLDialogElement.show())
     Did this via dialog.showModal() in js. COMPLETE
 onSubmit - Add book object to array (js function built) COMPLETE
-addCard to library that displays book info
--Green card is book that has been read
--Red card is unread book
--yellow card is currently being read
+addCard to library that displays book info COMPLETE
+//-Green card is book that has been read (Not doing these)
+//-Red card is unread book
+//-yellow card is currently being read
 
 Each card has a button to "remove from library":
 onClick - delete object from array and card from page
+(I need to create an id in the HTML that associates the "card" with the array item. This id needs to change if an item ahead of it changes. That way it continues to reflect the books position within the array.)
+Query all delete buttons.
+On-click grab id # for that object (Array Index)
+Delete that array item (splice)
+Delete the card containing that items info. (class="book#")
 
 Each card has a button to change the "read" status:
 onClick - change read value (changeStatus)
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("button");
+const showButton = document.querySelector(".addBook");
 const closeButton = document.querySelector("dialog #submit");
 const newBookForm = document.querySelector("form");
 let newTitle = document.getElementById("title");
@@ -8,6 +8,8 @@ let newAuthor = document.getElementById("author");
 let newPages = document.getElementById("pages");
 let newReadStatus = document.getElementById("read");
 const container = document.querySelector('.container');
+let bookCount = 0;
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -20,26 +22,14 @@ function Book(title, author, pages, read) {
   }
 
 function addBookToLibrary(title, author, pages, read) {
-  console.log(myLibrary.length);
+  //console.log(myLibrary.length);
   let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook)
-  console.log(myLibrary.length);
-  const bookBox = document.createElement('div'); //Create div for the book details to land in.
-  container.append(bookBox);
-  bookBox.setAttribute('class', 'bookBox');
-  bookBox.setAttribute('id', 'book' + myLibrary.length); //Assign id for which book it is for possible deletion later.
-  bookBox.innerHTML = `
-    <h3 class="bookTitle"> ${title} </h3>
-    <p class="bookAuthor"> ${author} </p>
-    <p class="bookPages">${pages}</p>
-    <button class="bookStatusChange">${read}</button>
-    <button class="bookRemove">Delete</button>`; 
+  myLibrary.push(newBook);
 }
   
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 pages", "Read")
 addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "Too Many", "Reading")
 
-console.log(myLibrary)
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
@@ -50,5 +40,42 @@ document.getElementById('submit').addEventListener('click', function(event) {
   addBookToLibrary(newTitle.value , newAuthor.value, newPages.value, newReadStatus.value);
   console.log(myLibrary);
   newBookForm.reset();
+  libraryLoop(bookCount);
 })
-   
+
+function displayBook(title, author, pages, read, i) { 
+  const bookBox = document.createElement('div'); //Create div for the book details to land in.
+  container.append(bookBox);
+  bookBox.setAttribute('class', 'bookBox');
+  bookBox.setAttribute('id', 'book' + i); //Assign id for which book it is for possible deletion later.
+  bookBox.innerHTML = `
+    <h3 class="bookTitle"> ${title} </h3>
+    <p class="bookAuthor"> ${author} </p>
+    <p class="bookPages">${pages}</p>
+    <button class="bookStatusChange">${read}</button>
+    <button class="bookRemove" id="${i}">Delete</button>`; 
+}
+
+function libraryLoop(i) {
+  while(i < myLibrary.length) {
+    //console.log(myLibrary[i]);
+    let data = myLibrary[i];
+    //console.log(data);
+    let {title, author, pages, read} = data
+    displayBook(title, author, pages, read, i);
+    i++;
+  }
+  bookCount = myLibrary.length;
+  console.log(bookCount);
+}
+
+libraryLoop(bookCount);
+
+let deletionButton = document.querySelectorAll('.bookRemove');
+
+deletionButton.forEach(button => button.addEventListener('click', function() {
+  bookNum = button.getAttribute('id');
+  console.log(bookNum);
+  myLibrary.splice(bookNum, 1);
+  console.log(myLibrary)
+}));
